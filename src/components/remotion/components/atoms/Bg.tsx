c2c5a1cc769510d69382ee44c0d8ee9911609e43
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2024-03-07 21:31:52 +0200

    .

--- src/components/remotion/components/atoms/Bg.tsx
+++ src/components/remotion/components/atoms/Bg.tsx
@@ -6,9 +6,11 @@ const Bg = ({ src, style }: { src: string; style?: object }) => {
    className="absolute inset-0 bg-cover bg-center -z-10 flex items-center justify-center"
    style={style}
   >
-   <div className="relative w-max h-full flex items-center justify-center">
-    <div className="w-full h-full backdrop-blur-md bg-white/10 absolute" />
-    <Img src={src} className="h-full w-auto max-w-none scale-[3]" />
+   <div className="relative w-max h-full flex items-center justify-center bg-white overflow-hidden">
+    <div className="w-full h-full backdrop-blur-sm bg-white/10 absolute z-10" />
+    <Img src={src} className="h-full w-auto max-w-none z-[5]" />
+    <div className="w-full h-full backdrop-blur-lg bg-white/10 absolute z-[1]" />
+    <Img src={src} className="h-full w-auto max-w-none scale-[3] absolute -z-1" />
    </div>
   </div>
  )
